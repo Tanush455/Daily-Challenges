1f8ced7a2bf1cb36236bbe038d4128e08fa042cb
Tanush <tanushdeshpande73@gmail.com>
2025-01-23 00:13:08 +0530

    other leetcode problem

---
+++ problem-2185.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int prefixCount(String[] words, String pref) {
+        int count = 0;
+        int n = pref.length();
+        for (String w : words) {
+            if (w.length() >= n && w.startsWith(pref)) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
